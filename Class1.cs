488905d6b15301e5b4d6c742f380d94ddcd19a0a
Blujai777 <148300175+Blujai777@users.noreply.github.com>
2024-07-22 16:57:42 -0400

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -18,28 +18,39 @@ namespace MoreTitleScreens
         private string customTitlesDirectory;
         public void OnEnable()
         {
+            On.RainWorld.PostModsInit += RainWorld_PostModsInit;
+        }
+        public void RainWorld_PostModsInit(On.RainWorld.orig_PostModsInit orig, RainWorld self)
+        {
             customTitlesFilePath = AssetManager.ResolveFilePath("customTitles.txt");
-            customTitlesDirectory = Path.GetDirectoryName(customTitlesFilePath);
+            customTitlesDirectory = Path.GetDirectoryName(customTitlesFilePath) + "/illustrations";
             Logger.LogInfo($"Resolved custom titles file path: {customTitlesFilePath}");
             Logger.LogInfo($"Custom titles directory: {customTitlesDirectory}");
             IL.Menu.IntroRoll.ctor += IntroRoll_ctor;
-
         }
+            
         public void IntroRoll_ctor(ILContext il)
         {
             var cursor = new ILCursor(il);
 
-            if (cursor.TryGotoNext(i => i.MatchNewarr<string>())
-                && cursor.TryGotoNext(MoveType.After, i => i.MatchStloc(3)))
+            if (cursor.TryGotoNext(i => i.MatchNewarr<string>()))
+               
             {
-                cursor.Emit(OpCodes.Ldloc_3);
-                cursor.EmitDelegate<Func<string[], string[]>>((oldTitleImages) => [.. oldTitleImages, "FFwhite", "FFred", "FFyellow", "Death", "DeathKarma",
+                if(cursor.TryGotoNext(MoveType.After, i => i.MatchStloc(3)))
+                {
+                    cursor.Emit(OpCodes.Ldloc_3);
+                    cursor.EmitDelegate<Func<string[], string[]>>((oldTitleImages) => [.. oldTitleImages, "FFwhite", "FFred", "FFyellow", "Death", "DeathKarma",
 "UnusedSpear", "Lights", "Dusk", "Night", "gormMurder", "Sky", "ScavMarks"]);
-                cursor.Emit(OpCodes.Stloc_3);
+                    cursor.Emit(OpCodes.Stloc_3);
+                }
+                else
+                {
+                    Logger.LogError("you already know(no stloc(3))");
+                }
             }
             else
             {
-                Logger.LogMessage("it isn't working");
+                Logger.LogMessage("can't find newarr");
             }
         }
     }
